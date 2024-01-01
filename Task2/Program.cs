e3f324bf235eef93bb546a36bc181653f69cc599
Murad <smart1992m@gmail.com>
2024-01-01 14:33:29 +0500

    2 Task

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = -3;
+int y = 3;
+
+if (x > 0 && y > 0)
+{
+    Console.Write("1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.Write("2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.Write("3");
+}
+else if (x > 0 && y < 0)
+{
+    Console.Write("4");
+}
